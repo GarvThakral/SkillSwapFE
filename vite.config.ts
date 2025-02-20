2f1dc9b064321c652e2c01bfd2742b2582fa61b3
Garv Thakral <garvthakralfx@gmail.com>
2025-02-20 15:08:04 +0530

    validation changes

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,16 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   build: {
-    chunkSizeWarningLimit: 800,
+    chunkSizeWarningLimit: 800, // Set the chunk size warning limit in KB
     rollupOptions: {
       output: {
         manualChunks(id) {
+          // Split '@dytesdk' related packages into a separate chunk
           if (id.includes('node_modules')) {
             if (id.includes('@dytesdk')) return 'dytesdk';
+            if (id.includes('@jitsi')) return 'jitsi-sdk';
             if (id.includes('react')) return 'react-vendor';
-            return 'vendor';
+            return 'vendor'; // Other node_modules dependencies go to 'vendor'
           }
         },
       },
