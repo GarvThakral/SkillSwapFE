1fb4b8aec5f9cd08df6a9a434b046f515c2137c0
Garv Thakral <garvthakralfx@gmail.com>
2025-02-20 15:03:41 +0530

    validation changes

--- src/components/myMeetingComponent.tsx
+++ src/components/myMeetingComponent.tsx
@@ -1,11 +1,16 @@
-import { DyteMeeting } from '@dytesdk/react-ui-kit';
+import { Suspense, lazy } from 'react';
+
+const DyteMeeting = lazy(() => import('@dytesdk/react-ui-kit').then(module => ({ default: module.DyteMeeting })));
 import { useDyteMeeting } from '@dytesdk/react-web-core';
+
 export function MyMeeting() {
   const { meeting } = useDyteMeeting();
 
   return (
     <div style={{ height: '684px' }}>
-      <DyteMeeting mode="fill" meeting={meeting} />
+      <Suspense fallback={<div>Loading meeting...</div>}>
+        <DyteMeeting mode="fill" meeting={meeting} />
+      </Suspense>
     </div>
   );
 }
